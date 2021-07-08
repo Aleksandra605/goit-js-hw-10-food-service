5d736ec51801e6aa87d3a45e9f809ff87972b913
Aleksandra605 <72212649+Aleksandra605@users.noreply.github.com>
2021-07-08 19:17:57 +0200

    изменила index.js

--- index.js
+++ index.js
@@ -11,27 +11,19 @@ listMenu.insertAdjacentHTML('beforeend', foodCard(menu));
 
 const switchToggle = document.querySelector('#theme-switch-toggle');
 const body = document.querySelector('body');
-
-function currentTheme() {
-  body.setAttribute('class', localStorage.getItem('theme'));
-  if (localStorage.getItem('theme') === Theme.DARK)
-    switchToggle.checked = 'true';
-}
-
-const changeTheme = function () {
-  if (body.classList.contains(Theme.LIGHT)) {
-    body.classList.remove(Theme.LIGHT);
-    body.classList.add(Theme.DARK);
-    localStorage.setItem('theme', Theme.DARK);
-  } else {
-    body.classList.remove(Theme.DARK);
-    body.classList.add(Theme.LIGHT);
-    localStorage.setItem('theme', Theme.LIGHT);
-  }
-};
-
-currentTheme();
 switchToggle.addEventListener('change', changeTheme);
 
-console.log(switchToggle.checked);
+body.setAttribute('class', localStorage.getItem('theme') || Theme.LIGHT);
+if (localStorage.getItem('theme') === Theme.DARK) switchToggle.checked = 'true';
+
+function changeTheme(e) {
+  if (e.target.checked) {
+    body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
 console.log(body);
